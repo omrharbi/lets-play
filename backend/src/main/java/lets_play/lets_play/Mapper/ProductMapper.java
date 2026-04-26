8117a3c72210a6c7947c47c38fa06d51b44dc040
omrharbi <rharbu.omar@gmail.com>
2026-04-26 22:32:03 +0100

    split some funcs

--- backend/src/main/java/lets_play/lets_play/Mapper/ProductMapper.java
+++ backend/src/main/java/lets_play/lets_play/Mapper/ProductMapper.java
@@ -9,8 +9,6 @@ import lets_play.lets_play.model.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-
     ProductResponse toResponse(Product product);
-
     List<ProductResponse> toResponseList(List<Product> products);
 }
